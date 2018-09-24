72a6c03d68558f62f05547dd9983bb951a1ccf3b
alexdenisova <37692145+alexdenisova@users.noreply.github.com>
2018-09-24 17:46:25 +0700

    Update fact.c

--- fact.c
+++ fact.c
@@ -2,12 +2,12 @@
 
 int main()
 {
-    int n;
-    scanf("%i", &n);
-    int r = n%10;
-    if(r<5) {
-        printf("%i\n", (n-r)/5);
-    }else{
-        printf("%i\n", (n-(r-5))/5);
-    }
+	int n;
+	scanf("%i", &n);
+	int r = n%10;
+	if(r<5) {
+		printf("%i\n", (n-r)/5);
+	}else{
+		printf("%i\n", (n-(r-5))/5);
+	}
 }
